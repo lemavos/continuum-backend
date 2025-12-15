60149ae0a20d9b533427bf92e7e9b9d404a50c13
lemavos <lemxvos.dev@proton.me>
2025-12-15 19:54:19 -0300

    first commit

---
+++ src/main/java/com/lemxvos/continuum/ContinuumApplication.java
@@ -0,0 +1,13 @@
+package com.lemxvos.continuum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lemxvos/continuum/ContinuumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemxvos.continuum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinuumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
